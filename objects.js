fd76a0a8fb9ea8a7f545fd3739225468b3e47495
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-08 17:01:01 +0000

    restrict the object types for function return

--- objects.js
+++ objects.js
@@ -1,9 +1,13 @@
 var user = {
     name: "Esraa",
     email: "esraa@gmail.com",
-    isActive: false
+    isActive: false,
 };
 function createUser(_a) {
     var string = _a.name, boolean = _a.isPaid;
 }
-createUser();
+createUser({ name: "Esraa", isPaid: true });
+function createCourse(_a) {
+    var string = _a.name, number = _a.price;
+    return { name: "ReactJS", price: 399 };
+}
--- objects.ts
+++ objects.ts
@@ -1,12 +1,13 @@
 const user = {
-    name: "Esraa",
-    email: "esraa@gmail.com",
-    isActive: false
+  name: "Esraa",
+  email: "esraa@gmail.com",
+  isActive: false,
 };
 
-function createUser({name: string, isPaid: boolean}){
+function createUser({ name: string, isPaid: boolean }) {}
 
+createUser({ name: "Esraa", isPaid: true });
+
+function createCourse(): { name: string, price: number } {
+  return { name: "ReactJS", price: 399 };
 }
-
-createUser();
-
