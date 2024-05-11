e347a9cebbab0208bfedcca80e0eb710a9927fcc
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-11 11:05:10 +0000

    Add private and public markers

--- purets/src/index.ts
+++ purets/src/index.ts
@@ -1,6 +1,10 @@
 class User {
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-    }
+  readonly city: string = "Cairo";
+
+  constructor(public email: string, public name: string, public userId: string) {
+    
+  }
 }
+
+const esraa = new User("esraa@gmail.com", "esraa", "123");
+//esraa.city = "Alex";
